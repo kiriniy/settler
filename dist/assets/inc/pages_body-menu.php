8d996a91054949da2843a146f8cf07f9f2b5c7a4
Kirill Nikitin <5729991+kiriniy@users.noreply.github.com>
2022-10-03 22:34:32 +0300

    Update pages_body-menu.php

--- dist/assets/inc/pages_body-menu.php
+++ dist/assets/inc/pages_body-menu.php
@@ -1,27 +1,13 @@
 
-    <!-- Example navigation (markup based on Bootstrap 3 framework) -->
+    <!-- Example navigation (markup based on Bootstrap 5 framework) -->
 
-    <nav class="navbar navbar-default">
+    <nav class="navbar navbar-expand-lg fixed-top bg-light">
       <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"><i class="fa fa-bars" aria-hidden="true"></i></button>
-          <a class="navbar-brand" href="<?php echo $base_url; ?>">Settler</a>
-        </div>
-    
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav navbar-right">
-            
-            <li <?php echo ($page_id == 'home') ? 'class="hidden"' : ''; ?>>
-              <a href="<?php echo $base_url; ?>"><i class="fa fa-home"></i></a>
-            </li>
-            
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Error Pages <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="assets/errors/404.php">Custom 404</a></li>
-                </ul>
-            </li>
-          </ul>
+
+        <a class="navbar-brand" href="#">Settler</a>
+
+        <div class="navbar-nav ms-auto">
+          <a class="nav-link" href="assets/errors/404.php">Custom 404</a>
         </div>
       </div>
     </nav>
