e6737dc3821f30fd4b4fa31a194b82d313b4b80c
Kirill Nikitin <5729991+kiriniy@users.noreply.github.com>
2023-08-11 06:36:01 +0300

    Create pages_head-noscript.php

---
+++ dist/assets/inc/pages_head-noscript.php
@@ -0,0 +1,14 @@
+
+    <!-- Display options for JavaScript-disabled browsers -->
+
+    <noscript>
+ 
+      <style>.no-js {display: none !important;}</style>
+ 
+      <!-- or another way (file are not included)
+
+      <link type="text/css" rel="stylesheet" href="assets/css/no-js.css">
+ 
+      -->
+ 
+    </noscript>
