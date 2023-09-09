8cebb6daa01ff8474a4aebf1662434a8898504c6
Kirill Nikitin <5729991+kiriniy@users.noreply.github.com>
2023-09-09 20:50:45 +0300

    Update pages_body-menu.php

--- dist/assets/inc/pages_body-menu.php
+++ dist/assets/inc/pages_body-menu.php
@@ -1,10 +1,10 @@
 
     <!-- Example navigation (markup based on Bootstrap 5 framework) -->
 
-    <nav class="navbar  fixed-top bg-body no-print">
+    <nav class="navbar fixed-top bg-body-tertiary border-bottom no-print">
       <div class="container">
 
-        <a class="navbar-brand" href="<?php echo $base_url; ?>">Settler</a>
+        <a class="navbar-brand" <?php if ($page_id != 'home') echo 'href="' . $base_url . '"'; ?>>Settler</a>
 
         <button class="navbar-toggler me-auto border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i class="bi bi-three-dots-vertical"></i></button>
 
@@ -15,6 +15,8 @@
             <a class="nav-link" href="assets/errors/404.php">Custom 404</a>
             <a class="nav-link" href="extras/blank.html">Blank HTML Template</a>
             <a class="nav-link" href="extras/blank.php">Blank PHP Template</a>
+            <a class="nav-link" href="extras/bs-theming.php">BS5 Theming</a>
+            <a class="nav-link" href="extras/bs-grid.php">BS5 Grid</a>
           </div>
         </div>
 
