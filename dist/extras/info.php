577b73cd838a41e945917a7946e3ff76a821c562
Кирилл Никитин <5729991+kiriniy@users.noreply.github.com>
2024-04-12 04:26:56 +0300

    Update info.php

--- dist/extras/info.php
+++ dist/extras/info.php
@@ -1,1 +1,22 @@
-<?php phpinfo();
+
+<?php $page_id      = '';
+      $page_ver     = '';
+      $page_path    = '';
+      $page_type    = '';
+      $page_class   = '';
+      $page_title   = '';
+      $page_desc    = '';
+
+      require '../assets/inc/pages_config.php';
+
+      if ($base_env != 'prod') {
+
+          phpinfo();
+
+      } else {
+
+          echo '<h1>Heads up!</h1>
+                <hr>
+                <p>The PHP function <strong>phpinfo()</strong> is disabled by default for the "prod" environment for security reasons. It displays information which can be used to compromise the server that your site is running on.</p>';
+
+      }
