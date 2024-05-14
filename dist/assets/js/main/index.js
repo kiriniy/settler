c5a26c7d7a6f63855a0f52b665a0741a6e2f4785
Кирилл Никитин <5729991+kiriniy@users.noreply.github.com>
2024-05-14 04:28:24 +0300

    Update index.js

--- dist/assets/js/main/index.js
+++ dist/assets/js/main/index.js
@@ -1,12 +1,12 @@
 
-// Settler's main.js builder
+// Settler's main.js builder.
 // In order to use custom build, you'll need a JavaScript bundler.
 
-import "./alerts/cookie-message";
+import "alerts/cookie-message";
 
-import "./bs-theme/color-modes";
-import "./bs-theme/color-switch";
+import "bs-theme/color-modes";
+import "bs-theme/color-switch";
 
-import "./conditions/if-dark";
-import './conditions/if-light';
-import "./conditions/if-mobile";
+import "conditions/if-dark";
+import 'conditions/if-light';
+import "conditions/if-mobile";
