bbc0b500d7071c126908d957a1e6ef41f7255961
Кирилл Никитин <5729991+kiriniy@users.noreply.github.com>
2025-01-10 20:08:31 +0300

    Create 500.php

---
+++ dist/assets/errors/500.php
@@ -0,0 +1,67 @@
+
+<?php $page_id      = '500';
+      $page_ver     = '?v=' . filemtime(__FILE__);
+      $page_type    = 'errors';
+      $page_class   = 'error-500';
+      $page_level   = '../../';
+      $page_path    = 'assets/errors/';
+
+      $page_lang    = 'en';
+      $page_title   = 'Internal Server Error';
+      $page_desc    = '';
+      
+      require $page_level . 'site.php';
+      
+      $page_csp        = '';
+      $page_robots     = '';
+      $page_autoformat = '';
+      $page_canonical  = '';
+      $page_head_attr  = '';
+
+      $page_notices    = false;
+
+      ?>
+
+<!DOCTYPE HTML>
+
+<html <?php echo $page_html_attr; ?>>
+  <head <?php echo $page_head_attr; ?>>
+
+    <?php include $assets . 'inc/pages_head-meta.php';
+          include $assets . 'inc/pages_head-resources.php';
+          include $assets . 'inc/pages_head-noscript.php';
+
+          ?>
+
+  </head>
+
+  <body <?php echo $page_body_attr; ?>>
+
+    <!-- PAGE CONTENT START -->
+
+    <?php include $assets . 'inc/pages_body-nav.php'; ?>
+
+    <!-- Example 500 content (markup based on Bootstrap 5 framework) -->
+
+    <main class="h-100 d-flex align-items-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <h1>Error 500
+            <br>Internal Server Error!</h1>
+            <p>Our server encountered an unexpected issue. We're already working on fixing it.
+            <br>You may go <a href='javascript:history.back(1)'>back</a> or <a href='<?php echo $base_url; ?>'>start over</a>.</p>
+            <hr>
+            <p class="small text-muted">Rename and edit <code>htaccess</code> file to activate custom error pages.</p>
+          </div>
+        </div>
+      </div>
+    </main>
+
+    <!-- PAGE CONTENT END -->
+
+    <?php include $assets . 'inc/pages_body-notices.php';
+          include $assets . 'inc/pages_body-scripts.php'; ?>
+
+  </body>
+</html>
