9c345323edd3836adf52a8a7f1a433adf87e6c4a
Кирилл Никитин <5729991+kiriniy@users.noreply.github.com>
2025-10-13 20:00:41 +0300

    Moving the source files to the /src folders. Bootstrap update to v5.3.7 and Bootstrap Icons update to v1.13.1

---
+++ dist/assets/js/src/index.bootstrap.js
@@ -0,0 +1,34 @@
+
+ /*!
+ * Bootstrap v5.3.7 (https://getbootstrap.com/)
+ * Copyright 2011-2025 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+ */
+
+import Alert from 'bootstrap/alert.js'
+import Button from 'bootstrap/button.js'
+import Carousel from 'bootstrap/carousel.js'
+import Collapse from 'bootstrap/collapse.js'
+import Dropdown from 'bootstrap/dropdown.js'
+import Modal from 'bootstrap/modal.js'
+import Offcanvas from 'bootstrap/offcanvas.js'
+import Popover from 'bootstrap/popover.js'
+import ScrollSpy from 'bootstrap/scrollspy.js'
+import Tab from 'bootstrap/tab.js'
+import Toast from 'bootstrap/toast.js'
+import Tooltip from 'bootstrap/tooltip.js'
+
+export default {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  Offcanvas,
+  Popover,
+  ScrollSpy,
+  Tab,
+  Toast,
+  Tooltip
+}
