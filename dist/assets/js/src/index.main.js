f82c90ec2ee5a0db0930887ea76d46fe46e01afa
Кирилл Никитин <5729991+kiriniy@users.noreply.github.com>
2025-10-14 14:59:16 +0300

    Refactoring js dir and main file

---
+++ dist/assets/js/src/index.main.js
@@ -0,0 +1,10 @@
+
+// Settler's main.js builder.
+// In order to use custom build, you'll need a JavaScript bundler.
+
+import "main/managers/cookie-alerts";
+import "main/managers/themes-switch";
+
+import "main/detectors/if-dark";
+import 'main/detectors/if-light';
+import "main/detectors/if-mobile";
